bfafe3728e6d220980d6198a40d40392b7416008
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-07-21 13:37:14 +0700

    Tugas Hari Selasa Penambahan Cara Penyelesaian

--- exercises6.js
+++ exercises6.js
@@ -23,7 +23,7 @@ function hitungHuruf(kata) {
 
 // TEST CASES
 console.log(hitungHuruf('Today, is the greatest day ever')); // greatest
-// console.log(hitungHuruf('I am a passionate developer')); // passionate
+console.log(hitungHuruf('I am a passionate developer')); // passionate
 // console.log(hitungHuruf('aku adalah anak gembala')); // adalah
 // console.log(hitungHuruf('rajin pangkal kaya')); // pangkal
 // console.log(hitungHuruf('mengayuh perahu di danau')); // danau
